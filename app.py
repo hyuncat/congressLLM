3e22cdb09caacf13340236d73d885433df37accf
Sarah Hong <ssh2198@columbia.edu>
2024-02-03 11:09:58 -0500

    Navigation, no <p>

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
+import re
+from datetime import datetime
+
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+# Hello route
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name=None):
+    return render_template (
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
 
 if __name__ == '__main__':
     app.run(debug=True)
