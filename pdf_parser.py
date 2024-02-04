6d0829605c83495e755275a66b4207b5bb6742ad
moisesmata <moisesmata2627@gmail.com>
2024-02-03 21:13:51 -0500

    updated pdf parser to classify whole document

--- pdf_parser.py
+++ pdf_parser.py
@@ -2,6 +2,40 @@ import requests
 from google.api_core.client_options import ClientOptions
 from google.cloud import documentai
 import pandas as pd
+import argparse
+import os
+from google.cloud import language_v2
+import numpy
+
+def classify(text, verbose=True):
+    """Classify the input text into categories."""
+
+    language_client = language_v2.LanguageServiceClient()
+
+    document = language_v2.Document(
+        content=text, type_=language_v2.Document.Type.PLAIN_TEXT
+    )
+    response = language_client.classify_text(request={"document": document})
+    categories = response.categories
+
+    result = {}
+
+    for category in categories:
+        # Turn the categories into a dictionary of the form:
+        # {category.name: category.confidence}, so that they can
+        # be treated as a sparse vector.
+        result[category.name] = category.confidence
+
+    if verbose:
+        print(text)
+        for category in categories:
+            print("=" * 20)
+            print("{:<16}: {}".format("category", category.name))
+            print("{:<16}: {}".format("confidence", category.confidence))
+
+    return result
+
+
 
 PROJECT_ID = "linen-marking-413218"
 LOCATION = "us"  # Format is 'us' or 'eu'
@@ -10,9 +44,7 @@ MIME_TYPE = "application/pdf"
 
 # Initialize Document AI client
 client_options = {"api_endpoint": f"{LOCATION}-documentai.googleapis.com"}
-client = documentai.DocumentProcessorServiceClient(
-    client_options=client_options
-)
+document_client = documentai.DocumentProcessorServiceClient(client_options=client_options)
 
 # Function from https://medium.com/@mimichen123/document-ai-parsing-with-python-gcp-b3b38c2cbab9
 def trim_text(text: str): 
@@ -39,7 +71,7 @@ def read_remote_pdf(url):
         request = {"name": parent, "raw_document": raw_document}
 
         # Use the Document AI client to process the sample form
-        result = client.process_document(request=request)
+        result = document_client.process_document(request=request)
 
         document_object = result.document
         
@@ -60,6 +92,19 @@ def read_remote_pdf(url):
         df = pd.DataFrame({'text_blocks': text_blocks})
         print(df)
         df.to_csv('output.csv', index=False)
-        return df
+        return text_blocks
+    
+
+# Instantiates a client
+#client1 = language_v2.LanguageServiceClient()
+
+
 
 df = read_remote_pdf('https://www.govinfo.gov/content/pkg/CREC-2024-01-31/pdf/CREC-2024-01-31-dailydigest.pdf')
+
+
+for i in df:
+    print(i)
+    print(classify(i, verbose=False))
+    print("")
+    print("")
--- testing.py
+++ testing.py
@@ -1,16 +1,16 @@
 import argparse
 import json
 import os
-from google.cloud import language_v1
+from google.cloud import language_v2
 import numpy
 
 def classify(text, verbose=True):
     """Classify the input text into categories."""
 
-    language_client = language_v1.LanguageServiceClient()
+    language_client = language_v2.LanguageServiceClient()
 
-    document = language_v1.Document(
-        content=text, type_=language_v1.Document.Type.PLAIN_TEXT
+    document = language_v2.Document(
+        content=text, type_=language_v2.Document.Type.PLAIN_TEXT
     )
     response = language_client.classify_text(request={"document": document})
     categories = response.categories
@@ -33,25 +33,16 @@ def classify(text, verbose=True):
     return result
 
 # Instantiates a client
-client = language_v1.LanguageServiceClient()
+client = language_v2.LanguageServiceClient()
 
 # The text to analyze
-text = "Immigration is a complex and multifaceted phenomenon that has played a pivotal role in shaping societies throughout history. It involves the movement of individuals from one country to another with the intent of establishing residence. People migrate for various reasons, including economic opportunities, fleeing persecution, or seeking a better quality of life. Immigration brings diversity to communities, contributing to cultural richness and fostering innovation. However, it also raises challenges related to integration, social cohesion, and resource distribution. Striking a balance between welcoming newcomers and addressing the concerns of host populations is crucial for creating inclusive and sustainable societies. Ultimately, a thoughtful and compassionate approach to immigration can lead to the building of stronger, more resilient communities on a global scale."
-document = language_v1.types.Document(
-    content=text, type_=language_v1.types.Document.Type.PLAIN_TEXT
-)
+text = "SALT MARRIAGE PENALTY ELIMINATION ACT; DENOUNCING THE HARMFUL, ANTI- AMERICAN ENERGY POLICIES OF THE BIDEN ADMINISTRATION"
 
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(
-    request={"document": document}
-).document_sentiment
+
 
 
 result = classify(text, verbose=False)
 
-
-print(f"Text: {text}")
-print(f"Sentiment: {sentiment.score}, {sentiment.magnitude}")
 print(result)
 
 
